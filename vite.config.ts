664c1d8ee0cfbd95d87dd54ba35154438eea279e
k-matsuda-kdl <k-matsuda@kdl.co.jp>
2022-05-16 11:32:03 +0900

    e2eコードカバレッジ設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import istanbul from 'vite-plugin-istanbul';
+const istanbul = require("vite-plugin-istanbul")
 
 // https://vitejs.dev/config/
 export default defineConfig({
